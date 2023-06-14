6257b0256812516a094953abeba47cc723be2283
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2023-06-14 19:59:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	let width=0;
+	let height=0;
+	constructor(width,height){
+		this.width=width;
+		this.height=height;
+	}
+	function getArea(){
+		return width*height;
+	}
+}
 
-class Square extends Animal {}
+class Square extends Animal {
+	function getPerimeter(){
+		return 4*this.width;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
